b6f2f57d679c4a6eea6286881510dc39adeb524a
Ernie Turner <ernie.turner@ironcorelabs.com>
2019-02-22 10:53:34 -0700

    Run shellcheck on script

--- .travis_scripts/cross-test.sh
+++ .travis_scripts/cross-test.sh
@@ -8,19 +8,19 @@ if [ -z "${TARGET}" ]; then
     cargo build --verbose
     cargo fmt -- --check
 # Cross doesn't have support for ios targets, so manually add/build them with cargo
-elif [ $IOS = 1 ]; then
-    rustup target add $TARGET
-    cargo build --target $TARGET
+elif [ "$IOS" = 1 ]; then
+    rustup target add "$TARGET"
+    cargo build --target "$TARGET"
 # Otherwise use cross to build for the specified argument
 else
-    cross build --target $TARGET
+    cross build --target "$TARGET"
 fi
 
 # Only run unit tests if architecture specifies that we should
-if [ $TEST = 1 ]; then
+if [ "$TEST" = 1 ]; then
     if [ -z "${TARGET}" ]; then
         cargo test --verbose
     else
-        cross test --target $TARGET
+        cross test --target "$TARGET"
     fi
 fi
