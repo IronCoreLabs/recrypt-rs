d7c786f955ceb68820b41f9e9d7891a009d7842f
Colt Frederickson <coltfred@gmail.com>
2019-04-03 14:01:19 -0600

    Add hashable instance for &[T]

--- src/internal/hashable.rs
+++ src/internal/hashable.rs
@@ -33,6 +33,21 @@ where
     }
 }
 
+
+impl<T> Hashable for &[T]
+where
+    T: Hashable,
+{
+    fn to_bytes(&self) -> ByteVector {
+        let mut result: Vec<u8> = Vec::new();
+        for t in self.iter() {
+            let mut bytes = t.to_bytes();
+            result.append(&mut bytes);
+        }
+        result
+    }
+}
+
 impl<T> Hashable for Vec<T>
 where
     T: Hashable,
