c787abfc0dee0efc5b7bc7cc6cf05862993bd7c0
tpmccallum <mistermac2008@gmail.com>
2020-04-17 22:03:32 +1000

    Adding serde to SixtyBytes struct

--- src/api_480.rs
+++ src/api_480.rs
@@ -921,7 +921,6 @@ fn gen_random_fp12<R: RandomBytesGen>(random_bytes: &R) -> Fp12Elem<Monty480> {
 }
 
 /// Wrapper around 60 byte array so what we can add Debug, Eq, etc
-#[cfg(feature = "serde")]
 big_array! {
     BigArray;
     60,
@@ -929,7 +928,6 @@ big_array! {
 #[derive(Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize), serde(crate = "serde_crate"))]
 struct SixtyBytes{
-    #[cfg(feature = "serde")]
     #[serde(with = "BigArray")]
     arr: [u8; Monty480::ENCODED_SIZE_BYTES],
 }
