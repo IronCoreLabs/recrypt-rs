0d0a0fd31fdbb4d01f57ba019ec4ca8351b1c5a3
tpmccallum <mistermac2008@gmail.com>
2020-04-17 22:48:02 +1000

    Adding serde to SixtyBytes struct

--- src/api_480.rs
+++ src/api_480.rs
@@ -938,27 +938,25 @@ struct SixtyBytes {
 
 impl SixtyBytes {
     fn bytes(&self) -> &[u8; Monty480::ENCODED_SIZE_BYTES] {
-        &self.arr.0
+        &self.arr[0]
     }
 }
 
 impl fmt::Debug for SixtyBytes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.arr.0.to_vec())
+        write!(f, "{:?}", self.arr[0]to_vec())
     }
 }
 
 impl fmt::LowerHex for SixtyBytes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", hex::encode(self.arr.0.to_vec()))
+        write!(f, "{}", hex::encode(self.arr[0]to_vec()))
     }
 }
 
 impl Default for SixtyBytes {
     fn default() -> Self {
-        SixtyBytes {
-            arr: [u8; 60],
-        }
+       arr: [u8; 60],
     }
 }
 
@@ -1033,7 +1031,7 @@ impl PublicKey {
         &[u8; Monty480::ENCODED_SIZE_BYTES],
         &[u8; Monty480::ENCODED_SIZE_BYTES],
     ) {
-        (&self.x.arr.0, &self.y.arr.0)
+        (&self.x.arr[0], &self.y.arr[0])
     }
 
     ///Augment the PublicKey so that messages encrypted to that key cannot be decrypted by this PublicKey's PrivateKey.
@@ -1061,7 +1059,7 @@ impl PrivateKey {
     pub const ENCODED_SIZE_BYTES: usize = Monty480::ENCODED_SIZE_BYTES;
 
     pub fn bytes(&self) -> &[u8; PrivateKey::ENCODED_SIZE_BYTES] {
-        &self.bytes.arr.0
+        &self.bytes.arr[0]
     }
 
     pub fn new(bytes: [u8; PrivateKey::ENCODED_SIZE_BYTES]) -> PrivateKey {
@@ -1088,8 +1086,8 @@ impl PrivateKey {
     }
     ///Convert the keys to Frs and either add or subtract them, then turn it back into a PrivateKey.
     fn augment(first: &PrivateKey, second: &PrivateKey, subtract: bool) -> PrivateKey {
-        let first_fr = Fr480::from(first.bytes.arr.0);
-        let second_fr = Fr480::from(second.bytes.arr.0);
+        let first_fr = Fr480::from(first.bytes.arr[0]);
+        let second_fr = Fr480::from(second.bytes.arr[0]);
         let fr_result = if subtract {
             first_fr - second_fr
         } else {
@@ -1103,7 +1101,7 @@ bytes_eq_and_hash!(PrivateKey);
 
 impl Hashable60 for PrivateKey {
     fn to_bytes_60(&self) -> [u8; 60] {
-        self.bytes.arr.0
+        self.bytes.arr[0]
     }
 }
 
