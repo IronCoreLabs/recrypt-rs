2d90cc4a9bf5ad5b92ba196431d81142606d4869
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2020-04-18 09:19:55 -0600

    fix rust format issues

--- src/internal/memlock.rs
+++ src/internal/memlock.rs
@@ -12,27 +12,27 @@ extern crate winapi;
 
 #[cfg(all(unix, not(target_arch = "wasm32")))]
 pub fn mlock<T: Sized>(cont: &T) {
-  let ptr: *const T = cont;
-  let size = std::mem::size_of::<T>();
-  unsafe {
-    libc::mlock(ptr as *mut libc::c_void, size);
-    #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-    libc::madvise(ptr, size, libc::MADV_NOCORE);
-    #[cfg(target_os = "linux")]
-    libc::madvise(ptr as *mut libc::c_void, size, libc::MADV_DONTDUMP);
-  }
+    let ptr: *const T = cont;
+    let size = std::mem::size_of::<T>();
+    unsafe {
+        libc::mlock(ptr as *mut libc::c_void, size);
+        #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+        libc::madvise(ptr, size, libc::MADV_NOCORE);
+        #[cfg(target_os = "linux")]
+        libc::madvise(ptr as *mut libc::c_void, size, libc::MADV_DONTDUMP);
+    }
 }
 
 #[cfg(all(windows, not(target_arch = "wasm32")))]
 pub fn mlock<T: Sized>(cont: &T) {
-  let addr: *const T = cont;
-  let len = std::mem::size_of::<T>();
-  unsafe {
-  ::winapi::um::memoryapi::VirtualLock(
-    addr as ::winapi::shared::minwindef::LPVOID,
-    len as ::winapi::shared::basetsd::SIZE_T,
-  );
-  }
+    let addr: *const T = cont;
+    let len = std::mem::size_of::<T>();
+    unsafe {
+        ::winapi::um::memoryapi::VirtualLock(
+            addr as ::winapi::shared::minwindef::LPVOID,
+            len as ::winapi::shared::basetsd::SIZE_T,
+        );
+    }
 }
 
 #[cfg(any(target_arch = "wasm32", all(not(unix), not(windows))))]
@@ -40,56 +40,56 @@ pub fn mlock<T: Sized>(cont: &T) {}
 
 #[cfg(all(unix, not(target_arch = "wasm32")))]
 pub fn munlock<T: Sized>(cont: &T) {
-  let ptr: *const T = cont;
-  let size = std::mem::size_of::<T>();
-  unsafe {
-    libc::munlock(ptr as *mut libc::c_void, size);
-    #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-    libc::madvise(ptr, size, libc::MADV_CORE);
-    #[cfg(target_os = "linux")]
-    libc::madvise(ptr as *mut libc::c_void, size, libc::MADV_DODUMP);
-  }
+    let ptr: *const T = cont;
+    let size = std::mem::size_of::<T>();
+    unsafe {
+        libc::munlock(ptr as *mut libc::c_void, size);
+        #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+        libc::madvise(ptr, size, libc::MADV_CORE);
+        #[cfg(target_os = "linux")]
+        libc::madvise(ptr as *mut libc::c_void, size, libc::MADV_DODUMP);
+    }
 }
 
 #[cfg(all(windows, not(target_arch = "wasm32")))]
 pub fn munlock<T: Sized>(cont: &T) {
-  let addr: *const T = cont;
-  let len = std::mem::size_of::<T>();
-  unsafe {
-  ::winapi::um::memoryapi::VirtualUnlock(
-    addr as ::winapi::shared::minwindef::LPVOID,
-    len as ::winapi::shared::basetsd::SIZE_T,
-  );
-  }
+    let addr: *const T = cont;
+    let len = std::mem::size_of::<T>();
+    unsafe {
+        ::winapi::um::memoryapi::VirtualUnlock(
+            addr as ::winapi::shared::minwindef::LPVOID,
+            len as ::winapi::shared::basetsd::SIZE_T,
+        );
+    }
 }
 
 #[cfg(any(target_arch = "wasm32", all(not(unix), not(windows))))]
 pub fn munlock<T: Sized>(cont: &T) {}
 
 fn size_of_slice<T: Sized>(slice: &[T]) -> usize {
-  slice.len() * std::mem::size_of::<T>()
+    slice.len() * std::mem::size_of::<T>()
 }
 
 #[cfg(all(unix, not(target_arch = "wasm32")))]
 pub fn mlock_slice<T: Sized>(cont: &[T]) {
-  let size = size_of_slice(cont);
-  unsafe {
-    let ptr = cont.as_ptr() as *mut libc::c_void;
-    libc::mlock(ptr, size);
-    #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-    libc::madvise(ptr, size, libc::MADV_NOCORE);
-    #[cfg(target_os = "linux")]
-    libc::madvise(ptr, size, libc::MADV_DONTDUMP);
-  }
+    let size = size_of_slice(cont);
+    unsafe {
+        let ptr = cont.as_ptr() as *mut libc::c_void;
+        libc::mlock(ptr, size);
+        #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+        libc::madvise(ptr, size, libc::MADV_NOCORE);
+        #[cfg(target_os = "linux")]
+        libc::madvise(ptr, size, libc::MADV_DONTDUMP);
+    }
 }
 
 #[cfg(all(windows, not(target_arch = "wasm32")))]
 pub fn mlock_slice<T: Sized>(cont: &[T]) {
-  unsafe {
-    let addr = cont.as_ptr() as ::winapi::shared::minwindef::LPVOID;
-    let len = size_of_slice(cont);
-    ::winapi::um::memoryapi::VirtualLock(addr, len as ::winapi::shared::basetsd::SIZE_T);
-  }
+    unsafe {
+        let addr = cont.as_ptr() as ::winapi::shared::minwindef::LPVOID;
+        let len = size_of_slice(cont);
+        ::winapi::um::memoryapi::VirtualLock(addr, len as ::winapi::shared::basetsd::SIZE_T);
+    }
 }
 
 #[cfg(any(target_arch = "wasm32", all(not(unix), not(windows))))]
@@ -97,27 +97,27 @@ pub fn mlock_slice<T: Sized>(cont: &[T]) {}
 
 #[cfg(all(unix, not(target_arch = "wasm32")))]
 pub fn munlock_slice<T: Sized>(cont: &[T]) {
-  let size = size_of_slice(cont);
-  unsafe {
-    let ptr = cont.as_ptr() as *mut libc::c_void;
-    libc::munlock(ptr, size);
-    #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-    libc::madvise(ptr, size, libc::MADV_CORE);
-    #[cfg(target_os = "linux")]
-    libc::madvise(ptr, size, libc::MADV_DODUMP);
-  }
+    let size = size_of_slice(cont);
+    unsafe {
+        let ptr = cont.as_ptr() as *mut libc::c_void;
+        libc::munlock(ptr, size);
+        #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+        libc::madvise(ptr, size, libc::MADV_CORE);
+        #[cfg(target_os = "linux")]
+        libc::madvise(ptr, size, libc::MADV_DODUMP);
+    }
 }
 
 #[cfg(all(windows, not(target_arch = "wasm32")))]
 pub fn munlock_slice<T: Sized>(cont: &[T]) {
-  unsafe {
-    let addr = cont.as_ptr() as ::winapi::shared::minwindef::LPVOID;
-    let len = size_of_slice(cont);
-    ::winapi::um::memoryapi::VirtualUnlock(
-      addr as ::winapi::shared::minwindef::LPVOID,
-      len as ::winapi::shared::basetsd::SIZE_T,
-    );
-  }
+    unsafe {
+        let addr = cont.as_ptr() as ::winapi::shared::minwindef::LPVOID;
+        let len = size_of_slice(cont);
+        ::winapi::um::memoryapi::VirtualUnlock(
+            addr as ::winapi::shared::minwindef::LPVOID,
+            len as ::winapi::shared::basetsd::SIZE_T,
+        );
+    }
 }
 
 #[cfg(any(target_arch = "wasm32", all(not(unix), not(windows))))]
