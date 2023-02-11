c1acac378b552f79330e2b68d8d4381e6f30702d
Colt Frederickson <coltfred@gmail.com>
2023-02-10 19:53:49 -0700

    Add nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A build for cloaked search proxy.";
+  description = "A development environment for a rust project.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -15,29 +15,10 @@
         rusttoolchain =
           pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in rec {
-        # `nix build`
-        packages = {
-          cloaked-search-proxy = pkgs.rustPlatform.buildRustPackage {
-            pname = "cloaked-search-proxy";
-            version = "0.1.0";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-            cargoLock.outputHashes = {
-              "base64_type-0.1.0" = "nQLg2cyNG+RKxG1DpmkWuobxRMLSHimYjvde84k/31A=";
-              "phonetic-normalizer-0.1.0" = "1o49Wnu/tyLzjhuUK1GceM1UGjlR8aC9u0MVGQILud4=";
-            };
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            buildInputs = [ rusttoolchain pkgs.libiconv ]
-              ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-              [ pkgs.darwin.apple_sdk.frameworks.Security ];
-          };
-        };
-        defaultPackage = packages.cloaked-search-proxy;
-
         # nix develop
         devShell = pkgs.mkShell {
           buildInputs = with pkgs;
-            [ rusttoolchain pkg-config pkgs.libiconv ]
+            [ rusttoolchain pkg-config ]
             ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
             [ pkgs.darwin.apple_sdk.frameworks.Security ];
         };
