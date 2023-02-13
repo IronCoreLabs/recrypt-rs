fe4cc1553eda3c04345a9331e2f2bf88ca962f89
Colt Frederickson <coltfred@gmail.com>
2023-02-13 11:58:49 -0700

    Update MSRV to 1.60, add nix flake (#177)
    * Add nix flake
    
    * Update gitignore
    
    * Add nix flake
    
    * Update to our toolchain action
    
    * Update to our toolchain action
    
    * Bump MSRV to 1.60 and remove deprecated toolchain args
    
    * Rust format component is needed
    
    * Fully specify rust version
    
    * Update changelog

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A development environment for a rust project.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rusttoolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in rec {
+        # nix develop
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [ rusttoolchain pkg-config ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+            [ pkgs.darwin.apple_sdk.frameworks.Security ];
+        };
+
+      });
+}
